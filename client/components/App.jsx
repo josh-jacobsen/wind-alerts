d58413d11fe1c9869ec65dcf71558f59ed722599
josh-jacobsen <joshjacobsen5@gmail.com>
2016-08-30 22:51:59 +1200

    refactored nav bar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,50 +5,37 @@ import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
 import Wind from '../containers/windContainer.js'
 
-// const navbarInstance = (
-//   <Navbar>
-//     <Navbar.Header>
-//       <Navbar.Brand>
-//         <a href="#">React-Bootstrap</a>
-//       </Navbar.Brand>
-//     </Navbar.Header>
-//     <Nav>
-//       <NavItem eventKey={1} href="#">Link</NavItem>
-//       <NavItem eventKey={2} href="#">Link</NavItem>
-//       <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-//         <MenuItem eventKey={3.1}>Action</MenuItem>
-//         <MenuItem eventKey={3.2}>Another action</MenuItem>
-//         <MenuItem eventKey={3.3}>Something else here</MenuItem>
-//         <MenuItem divider />
-//         <MenuItem eventKey={3.3}>Separated link</MenuItem>
-//       </NavDropdown>
-//     </Nav>
-//   </Navbar>
-// )
+const NavbarInstance = React.createClass({
+  render: function() {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">WindyR</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavDropdown eventKey={3} title="Locations" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.1}>Cheltenham</MenuItem>
+            <MenuItem eventKey={3.2}>Takapuna</MenuItem>
+            <MenuItem eventKey={3.3}>Shoal Bay</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.3}>All locations</MenuItem>
+          </NavDropdown>
+          <NavItem eventKey={1} href="#">Forecast</NavItem>
+          <NavItem eventKey={2} href="#">About</NavItem>
+        </Nav>
+      </Navbar>
+    )
+  }
+})
 
 export default class App extends React.Component {
   render () {
     return (
       <div>
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">React-Bootstrap</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="#">Link</NavItem>
-          <NavItem eventKey={2} href="#">Link</NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.3}>Separated link</MenuItem>
-          </NavDropdown>
-        </Nav>
-      </Navbar>
-              <ul>
+        <NavbarInstance />
+        <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/wind">Wind</Link></li>
           <li><Link to="/">About</Link></li>
