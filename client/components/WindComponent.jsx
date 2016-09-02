19a13552bfaae8f26939b6a2fae40534e213cbaf
josh-jacobsen <joshjacobsen5@gmail.com>
2016-09-02 15:50:01 +1200

    reimplemented loading screen while fetching data

--- client/components/WindComponent.jsx
+++ client/components/WindComponent.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom'
 
 export default (props) => {
   const wind = props.wind
+  const success = props.status === 'success'
 
+  if (success) {
     return (
       <div>
       <h4 className="fick">Speed: {wind.speed}</h4>
@@ -11,5 +13,13 @@ export default (props) => {
       <h4>Gust: {wind.gust}</h4>
       </div>
     )
+  } else {
+    return (
+      <div>
+        <h4>Loading.....</h4>
+      </div>
+    )
+  }
+
 
 }
